00d1560dbc2eee8f12bc78ea549b873a31abe6b5
Nico Salvador <salvador.nico@gmail.com>
2018-11-06 22:05:06 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+#app
+	h1 Hello World
+	router-view
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { Component } from "vue-property-decorator"
+
+@Component({
+	metaInfo: {
+		titleTemplate: "%s | Vue Typescript Template",
+	},
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="stylus">
+#app h1
+	center(XY, absolute)
+</style>
