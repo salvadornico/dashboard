69bf8107123519b3f8a1be23d2198c61e7c4ca58
Nico Salvador <salvador.nico@gmail.com>
2018-11-08 21:03:17 +0800

    Added navbar with time

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template lang="pug">
 #app
-	h1 Hello World
-	v-btn(color="blue") Success
+	Navbar
 	router-view
 </template>
 
 <script lang="ts">
+import Navbar from "@/components/Navbar.vue"
 import Vue from "vue"
 import { Component } from "vue-property-decorator"
 
 @Component({
 	metaInfo: {
-		titleTemplate: "%s | Vue Typescript Template",
+		titleTemplate: "Dashboard",
 	},
+	components: { Navbar },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="stylus">
-#app h1
-	center(XY, absolute)
+#app
+	font-family "Roboto", sans-serif
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+v-toolbar(dark, color="indigo darken-4")
+	v-toolbar-title.white--text {{ dateToday }}
+	v-spacer
+	v-btn(icon)
+		v-icon settings
+</template>
+
+<script lang="ts">
+import { format } from "date-fns"
+import Vue from "vue"
+import { Component } from "vue-property-decorator"
+
+@Component
+export default class Navbar extends Vue {
+	now = new Date()
+
+	mounted() {
+		setInterval(() => {
+			this.now = new Date()
+		}, 1000)
+	}
+
+	get dateToday(): string {
+		return format(this.now, "d MMMM yyyy | kk:mm:ss")
+	}
+}
+</script>
