75a8f9713b55386b869c3cc11417564fa78d3a34
Nico Salvador <salvador.nico@gmail.com>
2018-11-08 21:17:18 +0800

    Added basic scrolling carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+slider.slider(animation="fade", width="100vw", height="100vh")
+	slider-item(v-for="(page, index) in pages", :key="index")
+		v-container
+			h1 {{ page }}
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { Slider, SliderItem } from "vue-easy-slider"
+import { Component } from "vue-property-decorator"
+
+@Component({
+	components: { Slider, SliderItem },
+})
+export default class Carousel extends Vue {
+	pages = [1, 2, 3]
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,4 @@
+import Carousel from "@/components/Carousel.vue"
 import Vue from "vue"
 import Meta from "vue-meta"
 import Router from "vue-router"
@@ -7,5 +8,11 @@ Vue.use(Meta)
 
 export default new Router({
 	mode: "history",
-	routes: [],
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: Carousel,
+		},
+	],
 })
